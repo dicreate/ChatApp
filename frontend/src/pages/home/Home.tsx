bd5cac8125efe910a36fd7addb55d9a19b05b55a
DmitriyKovchugo <dkovchugo@mail.ru>
2024-02-21 12:56:23 +0300

    adaptive size register and login pages

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -1,10 +1,39 @@
+import { useEffect, useState } from 'react';
 import { MessageContainer, Sidebar } from '../../components'
+import useConversation from '../../zustand/useConversation';
 
 function Home() {
+
+   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+   const { selectedConversation, setSelectedConversation } = useConversation();
+
+   useEffect(() => {
+      const handleResize = () => {
+         setWindowWidth(window.innerWidth);
+      };
+
+      // Добавляем обработчик события при монтировании компонента
+      window.addEventListener('resize', handleResize);
+
+      // Возвращаем функцию для очистки, которая удаляет обработчик события
+      return () => window.removeEventListener('resize', handleResize);
+   }, []); // Пустой массив зависимостей, чтобы эффект выполнялся только при монтировании и размонтировании
+
+
+
    return (
       <div className='flex sm:h-[550px] md: h-[650px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
-         <Sidebar />
-         <MessageContainer />
+         {
+            windowWidth > 580
+               ? (<>
+                  <Sidebar />
+                  <MessageContainer />
+               </>)
+               : (<>
+                  <Sidebar />
+                  <MessageContainer />
+               </>)
+         }
       </div>
    )
 }
