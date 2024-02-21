0a6e63d858dfa68830042e62cf126d9e82f3effa
DmitriyKovchugo <dkovchugo@mail.ru>
2024-02-21 13:36:46 +0300

    feat: hide sidebar on sm size

--- frontend/src/components/sidebar/Sidebar.tsx
+++ frontend/src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,12 @@
 import SearchInput from "./SearchInput"
 import Conversations from "./Conversations"
 import LogoutButton from "./LogoutButton"
+import useConversation from "../../zustand/useConversation";
 const Sidebar = () => {
+   const { selectedConversation } = useConversation();
+
    return (
-      <div className="border-r border-slate-500 p-4 flex flex-col">
+      <div className={`border-r border-slate-500 p-4 flex flex-col ${selectedConversation ? `max-sm:hidden` : ``} `}>
          <SearchInput />
          <div className="divider px-3"></div>
          <Conversations />
